0ebf87c5ee168425d16c2470429121ff764cc8e0
Maia Johnson <maiaj1306@gmail.com>
2022-10-31 06:31:22 -0500

    installed dependencies and added in base redux code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
+const count = (state = 0, action) => {
+    return state;
+}
+
+const storeInstance = createStore(count);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
