e12a93ac0ddd14873d83fef387650b07deab354d
Maia Johnson <maiaj1306@gmail.com>
2022-10-31 10:00:53 -0500

    added route paths for components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import axios from 'axios';
 import './App.css';
-import { useSelector } from "react-redux";
+import { Route, HashRouter as Router } from "react-router-dom";
 import Feeling from '../Feeling/Feeling';
 import Understanding from '../Understanding/Understanding';
 import Support from '../Support/Support';
@@ -9,21 +8,31 @@ import Comments from "../Comments/Comments";
 
 function App() {
 
-  let count = useSelector((store) => {
-    return store.count;
-  })
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-      </header>
-      <Feeling />
-      <Understanding />
-      <Support />
-      <Comments />
-        <h4>Count: {count}</h4>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+        </header>
+
+        <Route exact path="/">
+          <Feeling />
+        </Route>
+
+        <Route exact path="/understanding">
+          <Understanding />
+        </Route>
+        
+        <Route exact path="/support">
+          <Support />
+        </Route>
+
+        <Route exact path="/comments">
+          <Comments />
+        </Route>
+        
+      </div>
+    </Router>
   );
 }
 
