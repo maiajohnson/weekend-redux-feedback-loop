936c2322f7e6298a583f0c5baaccc6bc521fc42d
Maia Johnson <maiaj1306@gmail.com>
2022-10-31 06:37:19 -0500

    created reducer and store

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { useSelector } from "react-redux";
 
 function App() {
+
+  let count = useSelector((store) => {
+    return store.count;
+  })
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
+        
+        <h4>Count: {count}</h4>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, combineReducers } from "redux";
+import { applyMiddleware, createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
+import logger from "redux-logger";
 
 const count = (state = 0, action) => {
     return state;
 }
 
-const storeInstance = createStore(count);
+const storeInstance = createStore(
+    count,
+    
+    applyMiddleware(
+        logger
+    )
+    );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={storeInstance}><App /></Provider>
+, document.getElementById('root'));
 registerServiceWorker();
